12d0823f646defb9d38b460135d0f9db60c27c44
Mike Busch <busch.michael@gmail.com>
2015-03-19 08:15:07 -0500

    more stuffs

--- app/controllers/recipes.rb
+++ app/controllers/recipes.rb
@@ -1,46 +1,46 @@
 get "/all_recipes" do
-  puts params.inpect
+  puts params.inspect
   @recipes = Recipe.all
   erb :all_recipes
 end
 
 get "/show_recipe" do
-  puts params.inpect
+  puts params.inspect
   @recipe = Recipe.find(params[:id])
   erb :show_recipe
 end
 
 get "/new_recipe_form" do
-  puts params.inpect
+  puts params.inspect
   @recipe = Recipe.new
   erb :new_recipe_form
 end
 
 post "/create_recipe" do
-  puts params.inpect
+  puts params.inspect
   @recipe = Recipe.create(title: params[:title],
                           ingredients: params[:ingredients],
-                          directions: params[:directions])
+                          description: params[:description])
   redirect "/show_recipe?id=#{@recipe.id}"
 end
 
 get "/edit_recipe_form" do
-  puts params.inpect
+  puts params.inspect
   @recipe = Recipe.find(params[:id])
   erb :edit_recipe_form
 end
 
 post "/update_recipe" do
-  puts params.inpect
+  puts params.inspect
   @recipe = Recipe.find(params[:id])
   @recipe.update(title: params[:title],
                  ingredients: params[:ingredients],
-                 directions: params[:directions])
+                 description: params[:description])
   redirect "/show_recipe?id=#{@recipe.id}"
 end
 
 get "/delete_recipe" do
-  puts params.inpect
+  puts params.inspect
   recipe = Recipe.find(params[:id])
   recipe.destroy
   redirect "/all_recipes"
