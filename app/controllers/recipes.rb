967da7aaf03c3dfc0c3c73ba32aaa43382b44ca6
Mike Busch <busch.michael@gmail.com>
2015-04-09 08:36:49 -0500

    remove the puts's

--- app/controllers/recipes.rb
+++ app/controllers/recipes.rb
@@ -1,23 +1,19 @@
 get "/all_recipes" do
-  puts params.inspect
   @recipes = Recipe.all
   erb :all_recipes
 end
 
 get "/show_recipe" do
-  puts params.inspect
   @recipe = Recipe.find(params[:id])
   erb :show_recipe
 end
 
 get "/new_recipe_form" do
-  puts params.inspect
   @recipe = Recipe.new
   erb :new_recipe_form
 end
 
 post "/create_recipe" do
-  puts params.inspect
   @recipe = Recipe.create(title: params[:title],
                           ingredients: params[:ingredients],
                           description: params[:description])
@@ -25,13 +21,11 @@ post "/create_recipe" do
 end
 
 get "/edit_recipe_form" do
-  puts params.inspect
   @recipe = Recipe.find(params[:id])
   erb :edit_recipe_form
 end
 
 post "/update_recipe" do
-  puts params.inspect
   @recipe = Recipe.find(params[:id])
   @recipe.update(title: params[:title],
                  ingredients: params[:ingredients],
@@ -40,7 +34,6 @@ post "/update_recipe" do
 end
 
 get "/delete_recipe" do
-  puts params.inspect
   recipe = Recipe.find(params[:id])
   recipe.destroy
   redirect "/all_recipes"
