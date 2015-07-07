f05b9b7a02415186f033ebc9a4d1b4d8a78056fb
Mike Busch <busch.michael@gmail.com>
2015-07-07 15:07:07 -0500

    woo

--- app/controllers/recipes.rb
+++ app/controllers/recipes.rb
@@ -1,41 +1,65 @@
-get "/all_recipes" do
-  @recipes = Recipe.all
+# GET - reading things
+# POST - creating things
+# PUT - updating data
+# DELETE - for deleting things silly
+
+# ALL Routes will start with the name of the resource collection
+# /recipes
+# Routes referring to single recipes will use:
+# /recipes/:id
+# Edit and New are special... they deviate from this convention a tiny bit
+# /recipes/new
+# /recipes/:id/edit
+
+# INDEX
+get "/recipes" do
+  @recipes = Recipe.order(:id).all
   erb :all_recipes
 end
 
-get "/show_recipe" do
-  @recipe = Recipe.find(params[:id])
-  erb :show_recipe
-end
-
-get "/new_recipe_form" do
+# NEW
+get "/recipes/new" do
   @recipe = Recipe.new
   erb :new_recipe_form
 end
 
-post "/create_recipe" do
-  @recipe = Recipe.create(title: params[:title],
+# CREATE
+post "/recipes" do
+  @recipe = Recipe.new(title: params[:title],
                           ingredients: params[:ingredients],
                           description: params[:description])
-  redirect "/show_recipe?id=#{@recipe.id}"
+  if @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  else
+    erb :new_recipe_form
+  end
+
 end
 
-get "/edit_recipe_form" do
+# SHOW
+get "/recipes/:id" do
+  @recipe = Recipe.find(params[:id])
+  erb :show_recipe
+end
+
+# EDIT
+get "/recipes/:id/edit" do
   @recipe = Recipe.find(params[:id])
   erb :edit_recipe_form
 end
 
-post "/update_recipe" do
+# UPDATE
+put "/recipes/:id" do
   @recipe = Recipe.find(params[:id])
   @recipe.update(title: params[:title],
                  ingredients: params[:ingredients],
                  description: params[:description])
-  redirect "/show_recipe?id=#{@recipe.id}"
+  redirect recipe_path(@recipe)
 end
 
-get "/delete_recipe" do
+# DELETE
+delete "/recipes/:id" do
   recipe = Recipe.find(params[:id])
   recipe.destroy
-  redirect "/all_recipes"
+  redirect "/recipes"
 end
-
---
+++ app/helpers/path_helpers.rb
@@ -0,0 +1,5 @@
+helpers do
+  def recipe_path(recipe)
+    "/recipes/#{recipe.id}"
+  end
+end
