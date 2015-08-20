9c21bcc4fb487af3b59ad41c1e71aac8f1358037
Mike Busch <busch.michael@gmail.com>
2015-08-20 09:27:35 -0500

    add tag_names to recipe form
    this uses a virtual attribute called tag_names to interact with the
    underlying tags association, by using the names of the tags in a comma
    separated list.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,22 @@ class Recipe < ActiveRecord::Base
   validates :title, presence: true
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def tag_names=(tag_string)
+    tag_names = tag_string.split(",")
+    tags = tag_names.map do |name|
+      # normalize the name (strip all the leading and trailing whitespace)
+      name = name.strip.downcase
+      # if a tag with this name exists, use that
+      # otherwise, build a new tag
+      tag = Tag.find_by(name: name) || Tag.new(name: name)
+    end
+    self.tags = tags
+  end
+
+  def tag_names
+    # pluck the names from the tags association
+    # join them with commas
+    self.tags.pluck(:name).join(", ")
+  end
 end
