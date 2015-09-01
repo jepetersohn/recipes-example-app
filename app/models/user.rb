9827c891e1564c1a932742765b2f07f28a9c323f
Mike Busch <busch.michael@gmail.com>
2015-09-01 13:28:14 -0500

    use bcrypt for password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
+
+  def password
+    @password ||= BCrypt::Password.new(self.hashed_password)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
 end
