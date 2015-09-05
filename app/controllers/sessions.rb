146d023554ce684fa3803ade7669580e031b37a0
Mike Busch <busch.michael@gmail.com>
2015-09-05 11:34:56 -0500

    handle the login action
    I've also added a helper method that will return the user who's
    currently logged in, if there is a user_id in the session.

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,3 +1,18 @@
 get "/login" do
   erb :"sessions/login"
 end
+
+post "/login" do
+  # first, find the user based on the email from the form
+  user = User.find_by(email: params[:email])
+
+  # if we found a user AND the passwords match, THEN store their id in the session
+  if user && user.password == params[:password]
+    session[:user_id] = user.id
+    redirect root_path
+  else
+    # otherwise rerender the login form with an error message
+    @error = "Sorry, the credentials you provided are incorrect."
+    erb :"sessions/login"
+  end
+end
